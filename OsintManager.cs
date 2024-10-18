52749c65062a8a35bb6f4bf40e0b793a0f18c063
leonplis1993@outlook.com <58827641+Nexxus67@users.noreply.github.com>
2024-10-17 21:45:53 -0300

    First commit

---
+++ OsintManager.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Threading.Tasks;
+using RestSharp;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace OsintToolkit
+{
+    public class OsintManager
+    {
+        private readonly string _apiKey;
+
+        public OsintManager(string apiKey)
+        {
+            _apiKey = apiKey;
+        }
+
+        public async Task<DomainInfo> GetDomainInfoAsync(string domain)
+        {
+            var client = new RestClient("https://www.virustotal.com/vtapi/v2/domain/report");
+            var request = new RestRequest
+            {
+                Method = Method.Get
+            };
+
+            request.AddParameter("apikey", _apiKey); 
+            request.AddParameter("domain", domain);
+
+            RestResponse response = await client.ExecuteAsync(request);
+
+            if (response.IsSuccessful)
+            {
+                try
+                {
+                    var domainInfo = JsonConvert.DeserializeObject<DomainInfo>(response.Content);
+
+                    if (domainInfo == null)
+                    {
+                        throw new Exception("Error: Deserialization returned null.");
+                    }
+
+                    return domainInfo;
+                }
+                catch (JsonException jsonEx)
+                {
+                    Console.WriteLine($"Deserialization error: {jsonEx.Message}");
+                    Console.WriteLine($"Response: {response.Content}");
+                    throw new Exception("Error while deserializing API response.");
+                }
+            }
+            else
+            {
+                throw new Exception($"Error when obtaining domain information. Error code: {response.StatusCode}");
+            }
+        }
+    }
+
+    public class DomainInfo
+    {
+        public string Domain { get; set; }
+        public string Whois { get; set; }
+        public string[] Subdomains { get; set; }
+        public string VerboseMsg { get; set; }
+        public int ResponseCode { get; set; }
+        public List<Resolution> Resolutions { get; set; }
+    }
+
+    public class Resolution
+    {
+        [JsonProperty("ip_address")]
+        public string IpAddress { get; set; }
+
+        [JsonProperty("last_resolved")]
+        public DateTime LastResolved { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,68 @@
+
+### OSINT ToolKit Library for .NET
+
+The OSINT Toolkit is a nuget package designed to gather and analyze information about domains using various online services. Currently, it integrates with the VirusTotal API to fetch domain details, including WHOIS information and resolution history. This tool aims to aid in Open Source Intelligence (OSINT) investigations.
+
+### Features
+
+Retrieve domain information from VirusTotal.
+Access WHOIS data.
+List historical resolutions of the domain.
+
+### To-Do
+
+Integrate with Shodan API for device and service information.
+Add support for Have I Been Pwned API to check for data breaches.
+Implement additional OSINT tools for enhanced domain analysis.
+Optimize error handling and logging for better debugging.
+
+### Usage
+
+Download the package: 
+
+```bash
+dotnet add package OsintManager.Net
+```
+
+Then create an instance of OsintManager, like this for example: 
+
+```csharp
+using System;
+using OsintToolkit;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        string apiKey = "your-api-key"; 
+
+        var osintManager = new OsintManager(apiKey); 
+
+        try
+        {
+            var domainInfo = await osintManager.GetDomainInfoAsync("yourdomain.com");
+            Console.WriteLine($"Domain: {domainInfo.Domain}");
+            Console.WriteLine($"WHOIS: {domainInfo.Whois}");
+            Console.WriteLine("Resolutions:");
+            foreach (var resolution in domainInfo.Resolutions)
+            {
+                Console.WriteLine($"IP: {resolution.IpAddress}, Last Resolved: {resolution.LastResolved}");
+            }
+        }
+        catch (JsonException jsonEx)
+        {
+            Console.WriteLine($"JSON Error: {jsonEx.Message}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}
+
+```
+
+
+Make sure to replace the your-api-key with your virustotal key and replace yourdomain.com with the desired domain
